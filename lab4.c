254745d251eb972956c9df596af14eef5682e031
katemckk <67487564+katemckk@users.noreply.github.com>
2025-02-27 10:13:21 -0800

    Update lab4.c

--- lab4.c
+++ lab4.c
@@ -126,6 +126,94 @@ void TIMER0_Init(void)
 	TR0=0; // Stop Timer/Counter 0
 }
 
+void LCD_pulse (void)
+{
+	LCD_E=1;
+	Timer3us(40);
+	LCD_E=0;
+}
+
+void LCD_byte (unsigned char x)
+{
+	// The accumulator in the C8051Fxxx is bit addressable!
+	ACC=x; //Send high nible
+	LCD_D7=ACC_7;
+	LCD_D6=ACC_6;
+	LCD_D5=ACC_5;
+	LCD_D4=ACC_4;
+	LCD_pulse();
+	Timer3us(40);
+	ACC=x; //Send low nible
+	LCD_D7=ACC_3;
+	LCD_D6=ACC_2;
+	LCD_D5=ACC_1;
+	LCD_D4=ACC_0;
+	LCD_pulse();
+}
+
+void WriteData (unsigned char x)
+{
+	LCD_RS=1;
+	LCD_byte(x);
+	waitms(2);
+}
+
+void WriteCommand (unsigned char x)
+{
+	LCD_RS=0;
+	LCD_byte(x);
+	waitms(5);
+}
+
+void LCD_4BIT (void)
+{
+	LCD_E=0; // Resting state of LCD's enable is zero
+	// LCD_RW=0; // We are only writing to the LCD in this program
+	waitms(20);
+	// First make sure the LCD is in 8-bit mode and then change to 4-bit mode
+	WriteCommand(0x33);
+	WriteCommand(0x33);
+	WriteCommand(0x32); // Change to 4-bit mode
+
+	// Configure the LCD
+	WriteCommand(0x28);
+	WriteCommand(0x0c);
+	WriteCommand(0x01); // Clear screen command (takes some time)
+	waitms(20); // Wait for clear screen command to finsih.
+}
+
+void LCDprint(char * string, unsigned char line, bit clear)
+{
+	int j;
+
+	WriteCommand(line==2?0xc0:0x80);
+	waitms(5);
+	for(j=0; string[j]!=0; j++)	WriteData(string[j]);// Write the message
+	if(clear) for(; j<CHARS_PER_LINE; j++) WriteData(' '); // Clear the rest of the line
+}
+
+int getsn (char * buff, int len)
+{
+	int j;
+	char c;
+	
+	for(j=0; j<(len-1); j++)
+	{
+		c=getchar();
+		if ( (c=='\n') || (c=='\r') )
+		{
+			buff[j]=0;
+			return j;
+		}
+		else
+		{
+			buff[j]=c;
+		}
+	}
+	buff[j]=0;
+	return len;
+}
+
 void main (void) 
 {
 	unsigned long frequency;
@@ -141,6 +229,14 @@ void main (void)
 	        "Compiled: %s, %s\n\n",
 	        __FILE__, __DATE__, __TIME__);
 
+	char buff[17];
+	// Configure the LCD
+	LCD_4BIT();
+	
+   	// Display something in the LCD
+	LCDprint("Capacitance:", 1, 1);
+	LCDprint(" ", 2, 1);
+
 	while(1)
 	{
 		TL0=0;
@@ -152,10 +248,18 @@ void main (void)
 		TR0=0; // Stop Timer/Counter 0
 		frequency =overflow_count*0x10000L+TH0*0x100L+TL0;
 
+		
+
 		printf("\rf=%luHz", frequency);
 		capacitance = 1.44*3*1000*0.000001/(float)frequemcy;
 		printf("\rc=%f uf", capacitance);
 		
+		//printf("Type F to display frequency and P to display period: ");
+		//getsn(buff, sizeof(buff));
+		//printf("\n");
+		LCDprint("%f uF", capacitance, 2, 1);
+		
 	}
+
 	
 }
