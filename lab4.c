08783626fc9fd4e55670e8cd42815df650d175a7
katemckk <67487564+katemckk@users.noreply.github.com>
2025-02-26 22:04:06 -0800

    Update lab4.c

--- lab4.c
+++ lab4.c
@@ -1,13 +1,17 @@
+// FreqEFM8.c: Measure the frequency of a signal on pin T0.
+//
+// By:  Jesus Calvino-Fraga (c) 2008-2018
+//
+// The next line clears the "C51 command line options:" field when compiling with CrossIDE
+//  ~C51~
+  
 #include <EFM8LB1.h>
 #include <stdio.h>
-#include <math.h>
 
-#define SYSCLK      24500000L  // SYSCLK frequency in Hz
+#define SYSCLK      72000000L  // SYSCLK frequency in Hz
 #define BAUDRATE      115200L  // Baud rate of UART in bps
 
-void print_capacitance ();
-
-void display_cap();
+unsigned char overflow_count;
 
 char _c51_external_startup (void)
 {
@@ -19,21 +23,56 @@ char _c51_external_startup (void)
 	VDM0CN |= 0x80;
 	RSTSRC = 0x02;
 
-	// Use a 24.5MHz clock
-	SFRPAGE = 0x00;
-	CLKSEL  = 0x00;
-	CLKSEL  = 0x00;
-	while ((CLKSEL & 0x80) == 0);
+	#if (SYSCLK == 48000000L)	
+		SFRPAGE = 0x10;
+		PFE0CN  = 0x10; // SYSCLK < 50 MHz.
+		SFRPAGE = 0x00;
+	#elif (SYSCLK == 72000000L)
+		SFRPAGE = 0x10;
+		PFE0CN  = 0x20; // SYSCLK < 75 MHz.
+		SFRPAGE = 0x00;
+	#endif
+	
+	#if (SYSCLK == 12250000L)
+		CLKSEL = 0x10;
+		CLKSEL = 0x10;
+		while ((CLKSEL & 0x80) == 0);
+	#elif (SYSCLK == 24500000L)
+		CLKSEL = 0x00;
+		CLKSEL = 0x00;
+		while ((CLKSEL & 0x80) == 0);
+	#elif (SYSCLK == 48000000L)	
+		// Before setting clock to 48 MHz, must transition to 24.5 MHz first
+		CLKSEL = 0x00;
+		CLKSEL = 0x00;
+		while ((CLKSEL & 0x80) == 0);
+		CLKSEL = 0x07;
+		CLKSEL = 0x07;
+		while ((CLKSEL & 0x80) == 0);
+	#elif (SYSCLK == 72000000L)
+		// Before setting clock to 72 MHz, must transition to 24.5 MHz first
+		CLKSEL = 0x00;
+		CLKSEL = 0x00;
+		while ((CLKSEL & 0x80) == 0);
+		CLKSEL = 0x03;
+		CLKSEL = 0x03;
+		while ((CLKSEL & 0x80) == 0);
+	#else
+		#error SYSCLK must be either 12250000L, 24500000L, 48000000L, or 72000000L
+	#endif
 	
 	P0MDOUT |= 0x10; // Enable UART0 TX as push-pull output
 	XBR0     = 0x01; // Enable UART0 on P0.4(TX) and P0.5(RX)                     
-	XBR1     = 0X00;
+	XBR1     = 0X10; // Enable T0 on P0.0
 	XBR2     = 0x40; // Enable crossbar and weak pull-ups
 
+	#if (((SYSCLK/BAUDRATE)/(2L*12L))>0xFFL)
+		#error Timer 0 reload value is incorrect because (SYSCLK/BAUDRATE)/(2L*12L) > 0xFF
+	#endif
 	// Configure Uart 0
 	SCON0 = 0x10;
-	CKCON0 |= 0b_0000_1000 ; // Timer 1 uses the system clock.
-	TH1 = 0x100-((SYSCLK/BAUDRATE)/2L);
+	CKCON0 |= 0b_0000_0000 ; // Timer 1 uses the system clock divided by 12.
+	TH1 = 0x100-((SYSCLK/BAUDRATE)/(2L*12L));
 	TL1 = TH1;      // Init Timer1
 	TMOD &= ~0xf0;  // TMOD: timer 1 in 8-bit auto-reload
 	TMOD |=  0x20;                       
@@ -42,9 +81,81 @@ char _c51_external_startup (void)
 	
 	return 0;
 }
+ 
+// Uses Timer3 to delay <us> micro-seconds. 
+void Timer3us(unsigned char us)
+{
+	unsigned char i;               // usec counter
+	
+	// The input for Timer 3 is selected as SYSCLK by setting T3ML (bit 6) of CKCON0:
+	CKCON0|=0b_0100_0000;
+	
+	TMR3RL = (-(SYSCLK)/1000000L); // Set Timer3 to overflow in 1us.
+	TMR3 = TMR3RL;                 // Initialize Timer3 for first overflow
+	
+	TMR3CN0 = 0x04;                 // Sart Timer3 and clear overflow flag
+	for (i = 0; i < us; i++)       // Count <us> overflows
+	{
+		while (!(TMR3CN0 & 0x80));  // Wait for overflow
+		TMR3CN0 &= ~(0x80);         // Clear overflow indicator
+		if (TF0)
+		{
+		   TF0=0;
+		   overflow_count++;
+		}
+	}
+	TMR3CN0 = 0 ;                   // Stop Timer3 and clear overflow flag
+}
 
-void main(void) {
-  // main code goes here
-  
-  return 0;
+void waitms (unsigned int ms)
+{
+	unsigned int j;
+	for(j=ms; j!=0; j--)
+	{
+		Timer3us(249);
+		Timer3us(249);
+		Timer3us(249);
+		Timer3us(250);
+	}
+}
+
+void TIMER0_Init(void)
+{
+	TMOD&=0b_1111_0000; // Set the bits of Timer/Counter 0 to zero
+	TMOD|=0b_0000_0101; // Timer/Counter 0 used as a 16-bit counter
+	TR0=0; // Stop Timer/Counter 0
+}
+
+void main (void) 
+{
+	unsigned long frequency;
+	float capacitance;
+	
+	TIMER0_Init();
+
+	waitms(500); // Give PuTTY a chance to start.
+	printf("\x1b[2J"); // Clear screen using ANSI escape sequence.
+
+	printf ("EFM8 Frequency and Capacitance measurement using Timer/Counter 0.\n"
+	        "File: %s\n"
+	        "Compiled: %s, %s\n\n",
+	        __FILE__, __DATE__, __TIME__);
+
+	while(1)
+	{
+		TL0=0;
+		TH0=0;
+		overflow_count=0;
+		TF0=0;
+		TR0=1; // Start Timer/Counter 0
+		waitms(1000);
+		TR0=0; // Stop Timer/Counter 0
+		frequency =overflow_count*0x10000L+TH0*0x100L+TL0;
+
+		printf("\rf=%luHz", frequency);
+		capacitance = 1.44*3*1000*0.000001/(float)frequemcy;
+		printf("\rc=%f uf", capacitance);
+		
+	}
+	
 }
